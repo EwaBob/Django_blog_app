d77f2980f34b31c0f27f951ae427616c624dda7e
EwaBob <114256392+EwaBob@users.noreply.github.com>
2023-04-02 17:42:55 +0200

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,45 +17,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('', views.main),
-    # path('hello/', views.hello),
-    # path('add/', views.add),
-    # path('brothers/', views.brothers),
-    # path('fibonnaci/', views.fibonnaci),
-    # path('multiply/', views.multiply),
-    # path('game/', views.game),
-    # path('article/<int:id>/', views.article),
-    # path('greetings/<str:name>/<int:repeat>/', views.greetings),
-    # path('calc/<str:number_a>/<str:operation>/<str:number_b>/', views.calc),
-    # path('random_generator/<int:min>/<int:max>/', views.random_generator),
-    # path('random_generator/<int:min>/<int:max>/<int:throw>', views.random_generator),
-    # path('abc/', views.show_template),
-    # path('template/', views.first_template),
-    # path('fizz/', views.fizz_buzz),
-    # path('tab/', views.multiply1),
-    # path('form/', views.show_form),
-    # path('login/', views.login_user),
-    # path('addproduct/', views.add_product),
-    # path('product_show/', views.product_show),
-    # path('class/', views.FormView.as_view()),
-    # path('test/', views.TestView.as_view()),
-    # path('testshow/', views.ShowTestView.as_view()),
-    # path('pizza/', views.PizzaView.as_view()),
-    # path('car/', views.CarView.as_view()),
-    # path('login_class/', views.LoginView.as_view()),
-    # path('book/add/', views.BookAdd.as_view()),
-    # path('book/', views.book_list),
-    # path('book/edit/<int:pk>', views.BookEdit.as_view()),
-    # path('book/delete/<int:pk>', views.BookDelete.as_view()),
-    # path('article/add/', views.AddArticle.as_view()),
-    # path('article/', views.article_list),
-    # # path('article/<int:pk>/', views.blog_article_show),
-    # path('article/<int:pk>/', views.CommentAdd.as_view()),
+   
     path('blog/', include('blog_app1.urls'))
 
 ]
 
-#include przekierowanie do naszego nowego pliku
+
 
 
