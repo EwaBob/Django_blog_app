2eb4fe824aa61137f43b781c8af54229358b57a5
EwaBob <114256392+EwaBob@users.noreply.github.com>
2023-04-02 17:41:36 +0200

    Update urls.py

--- blog_app1/urls.py
+++ blog_app1/urls.py
@@ -7,10 +7,3 @@ urlpatterns = [
     path('', views.blog_index),
     path('article/<int:pk>/', views.ShowArticle.as_view()),
     path('add/', views.AddArticle.as_view()),
-]
-# urlpatterns = [
-#     path('article/add/', views.ArticleAdd.as_view()),
-#     path('article/', views.article_list),
-#     # path('article/<int:pk>/', views.blog_article_show),
-#     path('article/<int:pk>', views.CommentAdd.as_view()),
-#     ]
