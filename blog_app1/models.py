cc2dd887a91fc8ff4f5ae6b76072032bd98d900d
EwaBob <114256392+EwaBob@users.noreply.github.com>
2023-04-02 17:38:52 +0200

    Update models.py

--- blog_app1/models.py
+++ blog_app1/models.py
@@ -24,38 +24,6 @@ class Comment(models.Model):
     def __str__(self):
         return self.author + ":" + self.message
 
-# 1 do wielu
-# 1 artykułu
-# wiele komentarzy
-# gdzie postawić Foreign Key ? artyułu / komentarzy ?
-# komentarz1(FK) -> artykuł
-# komentarz2(FK) -> artykuł
 
-# class Article(models.Model):   #dziedzicze z modelu Django
-#     title = models.CharField(max_length=255)  #pole tekstowe
-#     author = models.CharField(max_length=128)
-#     description = models.TextField() #textfild nieograniczona ilosc znakow
-#
-# #metoda zwracajaca tytył atykułu
-#     def __str__(self):
-#         return self.title
-#
-#
-# class Comment(models.Model):
-#     author = models.CharField(max_length=128)
-#     message = models.CharField(max_length=255)
-#     # relacja wiele do jednego (wiele komenatrzy może wskazywać jeden artykuł)
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return self.author + ":" + self.message
-
-# class Comment(models.Model):
-#
-#     author = models.CharField(max_length=128)
-#     message = models.CharField(max_length=255)
-#     #relacja wiele do 1 (wiele komentarzy wskazuje na jeden artykuł)
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE)  #polaczenie obu modeli w relacji , do 1 artykułu wiele komentarzy(1 do wielu)
-#     #jezeli ktos skasuje artykuł to tez komentarze do niego CASCADE
 
 
